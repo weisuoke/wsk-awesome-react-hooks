f0a8eb58d1e0d282fcd38815c5ba3fe0174ec381
梅林的法杖 <530264127@qq.com>
2019-08-21 15:17:25 +0800

    first-upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Layout, Menu, Icon } from "antd";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from "./pages/Home";
+
+const { Header, Footer, Sider, Content } = Layout;
+const { SubMenu } = Menu;
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Layout style={{ minHeight: "100vh" }}>
+          <Header className="App__Header">Awesome React Hooks</Header>
+          <div className="main-wrapper">
+            <Layout className="App__Wrapper--Bottom">
+              <Router>
+                <Sider className="App__Slider">
+                  <Menu
+                    onClick={this.handleClick}
+                    defaultSelectedKeys={["1"]}
+                    defaultOpenKeys={["sub1"]}
+                    mode="inline"
+                  >
+                    <SubMenu
+                      key="sub1"
+                      title={
+                        <span>
+                          <Icon type="home" />
+                          <Link to="/home">Home</Link>
+                        </span>
+                      }
+                    >
+                      <Menu.Item key="1">Home</Menu.Item>
+                    </SubMenu>
+                    <SubMenu
+                      key="sub2"
+                      title={
+                        <span>
+                          <Icon type="github" />
+                          <span>Github</span>
+                        </span>
+                      }
+                    >
+                      <Menu.Item key="2">react-use</Menu.Item>
+                      <Menu.Item key="3">useHooks</Menu.Item>
+                    </SubMenu>
+                  </Menu>
+                </Sider>
+                <Content className="App__Content">
+                  <Route exact path="/home" component={Home} />
+                </Content>
+              </Router>
+            </Layout>
+          </div>
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
